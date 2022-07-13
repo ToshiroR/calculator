fe3eabd90a6c7a7b658a942a3ecbbc6c32bb5a28
Toshiro <toshiro3457@gmail.com>
2022-07-13 18:59:35 -0400

    event listener working with buttons

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const displayOne = document.querySelector(".display1");
+const displayTwo = document.querySelector(".display2");
+const btn = document.querySelectorAll("button");
 
+addEventListener('click', function(e) {
+
+  const target = e.target;
+  const keyText = target.textContent;
+  const action = target.dataset.action;
+  const display1 = displayOne.textContent;
+  const display2 = displayTwo.textContent;
+
+
+  if (!action) {
+    displayTwo.textContent = keyText
+  }
+
+  
+});
