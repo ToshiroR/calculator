ab1dfcbe39bec13ed6e1fc43a211d55c0035dc4a
Toshiro <toshiro3457@gmail.com>
2022-07-13 20:34:58 -0400

    eventListener working,need to work on how to get the buttons to display correctly

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
 const displayOne = document.querySelector(".display1");
 const displayTwo = document.querySelector(".display2");
-const btn = document.querySelectorAll("button");
-
-addEventListener('click', function(e) {
-
-  const target = e.target;
-  const keyText = target.textContent;
-  const action = target.dataset.action;
-  const display1 = displayOne.textContent;
-  const display2 = displayTwo.textContent;
+const calKey = document.querySelector('calculator-buttons')
+const buttons = document.querySelectorAll("button");
 
 
-  if (!action) {
-    displayTwo.textContent = keyText
+buttons.forEach(btn => {
+  
+  btn.addEventListener('click', function(e) {
+
+ 
+    const target = e.target;
+    const keyText = target.textContent;
+    const action = target.dataset.action;
+    let display1 = displayOne.textContent;
+    let display2 = displayTwo.textContent;
+
+
+    if (display2 === "0") {
+    displayOne.textContent = keyText;
+    displayTwo.textContent = keyText;
+  
+    } 
+    if (display2 !== "0") {
+    displayOne.textContent = display1;
+    displayTwo.textContent = display2 + keyText;
+    }
+
+    if (action === 'clear') {
+    displayOne.textContent = '';
+    displayTwo.textContent = '0';
   }
 
   
-});
+})
+
+})
